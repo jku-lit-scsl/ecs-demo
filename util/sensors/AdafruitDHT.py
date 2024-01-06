dfbfcc651250c30fe86f288ff0a763a9a50f98c4
Marco Stadler <marco.stadler@jku.at>
2024-01-06 14:12:05 +0100

    sensor bug
    Signed-off-by: Marco Stadler <marco.stadler@jku.at>

--- util/sensors/AdafruitDHT.py
+++ util/sensors/AdafruitDHT.py
@@ -6,5 +6,5 @@ from util.mqtt_forwarder import MQTTForwarder
 
 def collect_dht22_data(mqtt_fw: MQTTForwarder):
     while True:
-        mqtt_fw.publish('Temp={10.0} Humidity={15.0}')
+        mqtt_fw.publish('sensor', 'Temp={10.0} Humidity={15.0}')
         time.sleep(1)
