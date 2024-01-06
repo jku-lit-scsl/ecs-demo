41ba57f96ac6b71eb2c6baae1372165bed738d77
Marco Stadler <marco.stadler@jku.at>
2024-01-06 15:02:32 +0100

    removed legavy adafruit and added new sensor readings

--- Adafruit_Python_DHT/examples/AdafruitDHT.py
+++ Adafruit_Python_DHT/examples/AdafruitDHT.py
@@ -27,9 +27,9 @@ import Adafruit_DHT
 sensor_args = {'11': Adafruit_DHT.DHT11,
                '22': Adafruit_DHT.DHT22,
                '2302': Adafruit_DHT.AM2302}
-if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
-    sensor = sensor_args[sys.argv[1]]
-    pin = sys.argv[2]
+if True:
+    sensor = sensor_args['22']
+    pin = 4
 else:
     print('Usage: sudo ./Adafruit_DHT.py [11|22|2302] <GPIO pin number>')
     print('Example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO pin #4')
