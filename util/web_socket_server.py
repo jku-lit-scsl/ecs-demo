56dc6440031939b05cf5e9c0e5c2c3add2cae3eb
Marco Stadler <marco.stadler@jku.at>
2024-01-07 21:09:45 +0100

    refactored websocket server
    Signed-off-by: Marco Stadler <marco.stadler@jku.at>

--- util/web_socket_server.py
+++ util/web_socket_server.py
@@ -19,10 +19,12 @@ async def handle_msg_receive(websocket, path):
 
 
 async def _main_ws_starter():
-    start_server = websockets.serve(handle_msg_receive, host, 8765)
-    asyncio.get_event_loop().run_until_complete(start_server)
-    asyncio.get_event_loop().run_forever()
+    server = await websockets.serve(handle_msg_receive, host, 8765)
+
+    logging.info(f"WebSocket server started on ws://{host}:8765")
+
+    await server.wait_closed()
 
 
 def start_ws_server():
-    _main_ws_starter()
+    asyncio.run(_main_ws_starter())
