0f315365fa9e8dc4c98ebf4be11fffe85c6cfa44
Marco Stadler <marco.stadler@jku.at>
2024-01-08 17:57:42 +0100

    fixed circular bug
    Signed-off-by: Marco Stadler <marco.stadler@jku.at>

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-import time
-
 from secure_mod.defcon_handler import DefconHandler
 from util.mqtt_receiver import set_defcon_handler
-from util.setup import setup, get_mqtt_receiver
+from util.setup import setup
 
 if __name__ == '__main__':
     setup()
-    mqtt_receiver = get_mqtt_receiver()
     defcon_handler = DefconHandler()
     set_defcon_handler(defcon_handler)
-
-    for i in range(3):
-        defcon_handler.increase()
-        time.sleep(5)
